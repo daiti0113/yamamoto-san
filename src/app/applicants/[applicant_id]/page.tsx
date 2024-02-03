bb4ec29486e591803b2523d9dd2165ba07e14364
daiti0113 <stennis.ds.0113@gmail.com>
2024-02-04 00:21:39 +0900

    候補者の詳細情報を取得する処理を追加

--- src/app/applicants/[applicant_id]/page.tsx
+++ src/app/applicants/[applicant_id]/page.tsx
@@ -4,15 +4,25 @@ import { ScrollArea, ScrollBar } from "@/components/molecules/ScrollArea"
 import { KanbanBoard } from "@/components/organisms/KanbanBoard/KanbanBoard"
 import { DraggableItem } from "@/components/organisms/KanbanBoard/item"
 import { useTaskGroups } from "@/components/organisms/KanbanBoard/useTaskGroups"
+import { defaultApi } from "@/lib/api"
+import { calcAge } from "@/lib/date"
+import { useQuery } from "@tanstack/react-query"
 
-export default function ApplicantDetailPage() {
+export default function ApplicantDetailPage({ params: { applicant_id } }: { params: { applicant_id: string } }) {
+  const { data, isLoading } = useQuery({ queryKey: [`fetchApplicant_${applicant_id}`], queryFn: () => defaultApi.getUsersUserId(Number(applicant_id)).then((response) => response.data) })
+
   const kanbanBoardProps = useTaskGroups()
 
+  if (isLoading) return <div>Loading...</div>
+
   return (
-    <ScrollArea>
-      <KanbanBoard {...kanbanBoardProps} defaultTaskGroups={defaultTaskGroups} />
-      <ScrollBar orientation="horizontal" />
-    </ScrollArea>
+    <div>
+      <h1>{data?.firstName} {data?.lastName}（{data?.dateOfBirth ? calcAge(data?.dateOfBirth) : ""}歳）</h1>
+      <ScrollArea>
+        <KanbanBoard {...kanbanBoardProps} defaultTaskGroups={defaultTaskGroups} />
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+    </div>
   )
 }
 
