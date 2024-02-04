c51046b661b6e452f7db990666b073d51bbee8a8
daiti0113 <stennis.ds.0113@gmail.com>
2024-02-04 13:21:49 +0900

    カラムの追加ボタンをButtonコンポーネントに置き換え

--- src/components/organisms/KanbanBoard/NewColumnButton.tsx
+++ src/components/organisms/KanbanBoard/NewColumnButton.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/atoms/button";
 import { Plus } from "lucide-react"
 
 type NewColumnButtonProps = {
@@ -9,16 +10,15 @@ export const NewColumnButton: React.FC<NewColumnButtonProps> = ({
   updateShowModal
 }) => {
   return (
-    <button
+    <Button
       className="h-[103px] w-[335px] border border-dashed hover:underline"
-      onClick={() => {
-        updateShowModal(true)
-      }}
+      variant="ghost"
+      onClick={() => updateShowModal(true)}
     >
       <div className="flex items-center justify-center">
         <Plus className="h-4 w-4"></Plus>
         <span className="ml-2">Add column</span>
       </div>
-    </button>
+    </Button>
   )
 }
